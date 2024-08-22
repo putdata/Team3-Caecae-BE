2094347b5a1ca0b15c818d275d91c7c7293b956b
Jinwoo Park <wlsdndml213@gmail.com>
2024-08-22 10:40:26 +0900

    feat : 레디스 마스터-슬레이브 구조에서 읽기작업을 슬레이브에서 하도록 변경 (CC-177) (#44)
    * chore : 읽기전용 레디스를 사용하도록 환경 구성 (CC-177)
    
    * feat : 읽기전용 레디스에서 get메서드를 호출하도록 설정 (CC-177)

--- src/main/java/ai/softeer/caecae/global/config/RedisConfig.java
+++ src/main/java/ai/softeer/caecae/global/config/RedisConfig.java
@@ -22,6 +22,12 @@ public class RedisConfig {
     @Value("${REDIS_PASSWORD}")
     private String redisPassword;
 
+    @Value("${spring.data.redis.readonly-host}")
+    private String redisReadOnlyHost;
+
+    @Value("${spring.data.redis.readonly-port}")
+    private int redisReadOnlyPort;
+
     // 레디스 커넥션 생성
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
@@ -40,6 +46,27 @@ public class RedisConfig {
         template.setConnectionFactory(redisConnectionFactory());
         template.setKeySerializer(new StringRedisSerializer()); // 직렬화 가능하도록 설정
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // 직렬화 가능하도록 설정
+        return template;
+    }
+
+    // 읽기전용 레디스
+    @Bean
+    public RedisConnectionFactory redisReadOnlyConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(redisReadOnlyHost);
+        redisConfig.setPort(redisReadOnlyPort);
+        redisConfig.setPassword(redisPassword);
+
+        return new LettuceConnectionFactory(redisConfig);
+    }
+
+    // 읽기전용 레디스
+    @Bean
+    public RedisTemplate<String, Object> redisReadOnlyTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisReadOnlyConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 
--- src/main/java/ai/softeer/caecae/racinggame/repository/RacingGameInfoRepository.java
+++ src/main/java/ai/softeer/caecae/racinggame/repository/RacingGameInfoRepository.java
@@ -1,19 +1,28 @@
 package ai.softeer.caecae.racinggame.repository;
 
-import ai.softeer.caecae.global.enums.ErrorCode;
 import ai.softeer.caecae.racinggame.domain.entity.RacingGameInfo;
-import ai.softeer.caecae.racinggame.domain.exception.RacingGameException;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class RacingGameInfoRepository {
     // 레디스에 저장될 레이싱게임 정보의 키
     private final static String KEY = "racingGameInfo";
+
+    @Qualifier("redisTemplate")
     private final RedisTemplate<String, Object> redisTemplate;
+
+    @Qualifier("redisReadOnlyTemplate")
+    private final RedisTemplate<String, Object> redisReadonlyTemplate;
+
+    public RacingGameInfoRepository(
+            @Qualifier("redisTemplate") RedisTemplate<String, Object> redisTemplate,
+            @Qualifier("redisReadOnlyTemplate") RedisTemplate<String, Object> redisReadonlyTemplate) {
+        this.redisTemplate = redisTemplate;
+        this.redisReadonlyTemplate = redisReadonlyTemplate;
+    }
 
     // 레디스에 저장하는 로직
     public RacingGameInfo save(RacingGameInfo gameInfo) {
@@ -23,6 +32,6 @@ public class RacingGameInfoRepository {
 
     // 레디스에 저장된 객체를 가져오는 로직
     public RacingGameInfo get() {
-        return (RacingGameInfo) redisTemplate.opsForValue().get(KEY);
+        return (RacingGameInfo) redisReadonlyTemplate.opsForValue().get(KEY);
     }
 }
